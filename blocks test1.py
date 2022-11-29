57787d04ccf3ab7a8f057e86c41bed2eccdcd006
MartiPuigV <119410972+MartiPuigV@users.noreply.github.com>
2022-11-29 11:57:42 +0100

    Update blocks test1.py

--- blocks test1.py
+++ blocks test1.py
@@ -13,4 +13,3 @@ def divide_image(image, blocks):
         for j in range(w):
             blocks[i][j] = image[i * bH:(i + 1) * bH, j * bW:(j + 1) * bW]
     return blocks
-plt.plot(divide_image(DTU_image, blocks))
