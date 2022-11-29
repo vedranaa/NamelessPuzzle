81340b32f8b7827687e703f7bd28d63ebc03a52f
Vedrana Andersen Dahl <vedranaa@gmail.com>
2022-11-29 11:04:03 +0100

    Added DTU photo and matplotlib example

---
+++ matplotlib_example.py
@@ -0,0 +1,43 @@
+#%%
+import matplotlib.pyplot as plt
+import PIL.Image
+import numpy as np
+
+# reading and displaying the image
+filename = 'DTU_700x350.jpg'
+DTU_image = np.array(PIL.Image.open(filename))
+fig, ax = plt.subplots()
+ax.imshow(DTU_image)
+
+#%% extracting columns from the image
+print(DTU_image.shape)
+h, w, l = DTU_image.shape
+print(w//3)
+
+
+column0 = DTU_image[:, 0:233, :] 
+column1 = DTU_image[:, 233:466, :] 
+column2 = DTU_image[:, 466:, :] 
+
+mosaic = np.column_stack((column2, column1, column0))
+
+fig, ax = plt.subplots()
+ax.imshow(mosaic)
+
+
+#%% making a red background and pasting a part of the image
+red_image = np.zeros((350, 350, 3), dtype=np.uint8)
+red_image[:,:,0] = 255
+
+red_image[50:300, 50:300, :] = DTU_image[100:350, 100:350, :]
+
+fig, ax = plt.subplots()
+ax.imshow(red_image)
+
+# %%
+
+def extract_image_blocks(image, blocks_shape):
+    h, w, l = image.shape
+    
+
+
