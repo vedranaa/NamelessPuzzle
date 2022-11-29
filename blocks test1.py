7a847f3751882fc04e976f179f402e93e6d39564
MartiPuigV <119410972+MartiPuigV@users.noreply.github.com>
2022-11-29 11:54:26 +0100

    Her er blocks filen. Den virker ikke

---
+++ blocks test1.py
@@ -0,0 +1,16 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import PIL.Image
+filename = 'DTU_700x350.jpg'
+DTU_image = np.array(PIL.Image.open(filename))
+blocks = [[], [], [], []]
+def divide_image(image, blocks):
+    image_shape = 4, 6
+    h, w = image_shape
+    bH = h // w
+    bW = w // w
+    for i in range(h):
+        for j in range(w):
+            blocks[i][j] = image[i * bH:(i + 1) * bH, j * bW:(j + 1) * bW]
+    return blocks
+plt.plot(divide_image(DTU_image, blocks))
