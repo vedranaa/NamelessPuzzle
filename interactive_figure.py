f90fb090f3bac219a3ac837207a63b7f95493866
MartiPuigV <119410972+MartiPuigV@users.noreply.github.com>
2022-11-30 13:29:08 +0100

    dvubjkv
    dsvjb

--- interactive_figure.py
+++ interactive_figure.py
@@ -1,13 +1,25 @@
+<<<<<<< Updated upstream
 #%%
 import matplotlib
+=======
+>>>>>>> Stashed changes
 import matplotlib.pyplot as plt
-import PIL.Image
+import matplotlib.animation as animation
 import numpy as np
+from matplotlib.widgets import Button
+from itertools import count
 
+<<<<<<< Updated upstream
 matplotlib.use('Qt5Agg')  # choose a suitable backend 
+=======
+x_values = []
+y_values = []
+>>>>>>> Stashed changes
 
-def switch_images(images):
+fig, ax = plt.subplots()
+plt.subplots_adjust(bottom=0.25)
 
+<<<<<<< Updated upstream
     def arrow_navigation(event, i, I):
         if event.key == 'up':
             i = (i+1) % I
@@ -18,38 +30,48 @@ def switch_images(images):
         elif event.key == 'left':
             i = (i-1) % I
         return i
+=======
+index = count()
+>>>>>>> Stashed changes
 
-    def update_image():
-        ax.imshow(images[i])
-        ax.set_title(f'image {i}')
-        fig.canvas.draw()
-
-    def key_press(event):
-        nonlocal i
-        i = arrow_navigation(event, i, I)
-        update_image()
-
-    fig, ax = plt.subplots()
-    i = 0
-    I = len(images)
-    update_image()
-    fig.canvas.mpl_connect('key_press_event', key_press)
-    plt.axis('off')
-    plt.show()
+def update_animation(i):
+    ax.clear()
+    t = next(index)
+    x_values.append(t)
+    y_values.append(np.sin(t))
+    ax.plot(x_values, y_values)
 
 
+def start_animation(event):
+    global anim
+    anim = animation.FuncAnimation(fig, update_animation, interval=0)
+    anim.event_source.start()
+
+
+<<<<<<< Updated upstream
 image1 = np.array(PIL.Image.open('DTU_700x350.jpg'))
 cz = np.array(PIL.Image.open('captn_zenitsu.jpg'))
+=======
+def stop_animation(event):
+    global anim
+    anim.event_source.stop()
 
-image2 = cz[:350, :700]
-image3 = cz[350:700, 600:1300]
-image4 = cz[:350, 600:1300]
-image5 = cz[350:700, :700]
+>>>>>>> Stashed changes
 
-images = [image1, image2, image3, image4, image5]
+startax = plt.axes([0.3, 0.1, 0.1, 0.05])
+startbutton = Button(startax, label='Start', hovercolor='0.5')
 
+stopax = plt.axes([0.5, 0.1, 0.1, 0.05])
+stopbutton = Button(stopax, label='Stop', hovercolor='0.5')
+
+<<<<<<< Updated upstream
 switch_images(images)
 
 
    
 # %%
+=======
+startbutton.on_clicked(start_animation)
+stopbutton.on_clicked(stop_animation)
+plt.show()  
+>>>>>>> Stashed changes
